da56a6d879caf246244f0e6ca5e3ef51ee58890e
Kevin Smith <Kevin.Smith@microsoft.com>
2018-12-20 10:42:55 -0800

    Add repl tool

---
+++ tools/repl.js
@@ -0,0 +1,36 @@
+const { registerLoader } = require('../build/out/cli.js');
+const util = require('util');
+
+registerLoader();
+
+const { parse } = require('../packages/parser/src/index.js');
+const { compile } = require('../packages/compiler/src/index.js');
+
+function printAST(input, options) {
+  let result = parse(input, options);
+  console.log(util.inspect(result.ast, {
+    colors: true,
+    depth: 50,
+  }));
+}
+
+global.parse = parse;
+
+global.compile = compile;
+
+global.ast = function(strings, ...values) {
+  printAST(String.raw(strings, ...values), { module: true });
+};
+
+global.astScript = function(strings, ...values) {
+  printAST(String.raw(strings, ...values), { module: false });
+};
+
+global.skert = function(strings, ...values) {
+  let result = compile(String.raw(strings, ...values), {
+    module: true,
+    transformModules: true,
+  });
+
+  console.log(result.output);
+};
