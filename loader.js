de61e53d981af2a463f81e81404db2667fbda30c
Kevin Smith <Kevin.Smith@microsoft.com>
2018-12-21 12:36:54 -0800

    Always register the loader in "loader".

--- loader.js
+++ loader.js
@@ -1,7 +1,1 @@
-const { registerLoader } = require('./build/out/cli.js');
-
-try {
-  if (module.parent && module.parent.id === 'internal/preload') {
-    registerLoader();
-  }
-} catch (e) {}
+require('./build/out/cli.js').registerLoader();
